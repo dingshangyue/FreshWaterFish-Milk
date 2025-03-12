24cb09d5b3a5cbabeeedf67884bcec4248feb941
dingshangyue <152499991+dingshangyue@users.noreply.github.com>
2025-03-12 22:27:28 +0800

    Fix WorldGuard accessing AreaEffectCloud#potion; Fix Bukkit#getOnlinePlayers returns nothing; for #1711, #1684

--- arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/server/management/DedicatedPlayerListMixin.java
+++ arclight-common/src/main/java/io/izzel/arclight/common/mixin/core/server/management/DedicatedPlayerListMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
  */
 @Mixin(DedicatedPlayerList.class)
 public class DedicatedPlayerListMixin {
-    @Inject(method = "<init>", at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/server/players/PlayerList;<init>(Lnet/minecraft/server/MinecraftServer;Lnet/minecraft/core/LayeredRegistryAccess;Lnet/minecraft/world/level/storage/PlayerDataStorage;I)V"))
+    @Inject(method = "<init>", at = @At(value = "TAIL"))
     private void arclight$afterSuper(CallbackInfo ci) {
         ((CraftServerBridge)Bukkit.getServer()).bridge$setPlayerList((DedicatedPlayerList)(Object) this);
     }
