e07a206d7d97683b4b94bc9c986939b2e1d46426
dingshangyue <152499991+dingshangyue@users.noreply.github.com>
2026-02-14 11:48:52 +0800

    feat(paper): Add Paper configuration detection mark class

---
+++ arclight-common/src/main/java/com/destroystokyo/paper/PaperConfig.java
@@ -0,0 +1,10 @@
+package com.destroystokyo.paper;
+
+/**
+ * marker class for legacy Paper detection used by compatibility libraries
+ */
+public final class PaperConfig {
+
+    private PaperConfig() {
+    }
+}
---
+++ arclight-common/src/main/java/io/papermc/paper/configuration/Configuration.java
@@ -0,0 +1,10 @@
+package io.papermc.paper.configuration;
+
+/**
+ * marker class for Paper detection used by compatibility libraries
+ */
+public final class Configuration {
+
+    private Configuration() {
+    }
+}
